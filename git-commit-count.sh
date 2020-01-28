fc2594086e4bff9d9af6481f88fbc3ecd3beb486
yagawa <yagawa@qzc.co.jp>
2020-01-28 14:46:31 +0900

    Add execute command

--- git-commit-count.sh
+++ git-commit-count.sh
@@ -4,3 +4,6 @@
 date=$1
 
 git log --since=$date --numstat --pretty="%H"  | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("+%d, -%d\n", plus, minus)}'
+
+
+# $ curl -sf <this file path of raw> | sh -s
